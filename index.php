2eb7135fd12026d82f4461975a8136fda41dbbf0
Ulises <ua.huaman@gmail.com>
2017-04-20 07:19:38 +0200

    Este es una nueva modificación

--- index.php
+++ index.php
@@ -4,5 +4,6 @@ class Index
 	public function_construct(){
 	echo "Cambios hechos en la  rama nombre_rama";
 	echo "Este es el segundo cambio en la rama nombre_rama";
+	echo "Este es una tercera modificación";
 }
 }
