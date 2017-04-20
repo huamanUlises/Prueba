4651d46d15d8181dc475be2e87000922e4e691b3
Ulises <ua.huaman@gmail.com>
2017-04-20 06:27:03 +0200

    modigicado hay

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-Init README
+Este es la rama nombre_rama
+que una rama del proyecto principal "master"
--- index.php
+++ index.php
@@ -2,5 +2,7 @@
 class Index
 {
 	public function_construct(){
+	echo "Cambios hechos en la  rama nombre_rama";
+	echo "Este es el segundo cambio en la rama nombre_rama";
 }
 }
